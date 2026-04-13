e337821d37dd8a8bbf5a13b1511978d87ab098fc
Dogen <setratosfer@gmail.com>
2026-04-13 13:43:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 export const CONFIG = {
   BET_AMOUNT: 5,
-  MAX_BETS: 3,
-  MAX_COINS: 3,
+  MAX_BETS: 10,
+  MAX_COINS: 10,
 
   // ===== DIFF =====
-  MIN_DIFF_SHORT: 0.002,  // 0.2% (CTM)
-  MIN_DIFF_LONG: 0.004,   // 0.4% (Event)
+  MIN_DIFF_SHORT: 0.0025,  // 0.2% (CTM)
+  MIN_DIFF_LONG: 0.0025,   // 0.4% (Event)
 
   // ===== TRIGGER =====
-  TRIGGER_SHORT: 30, // detik sebelum close
-  TRIGGER_LONG: 60,  // lebih aman untuk event
+  TRIGGER_SHORT: 20, // detik sebelum close
+  TRIGGER_LONG: 20,  // lebih aman untuk event
 
   // ===== LIMIT =====
   MAX_DELAY: 60 * 60, // 1 jam (short)
