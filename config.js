7b7a7852ec35dd24227559e75d73ceb02b8a7761
Dogen <setratosfer@gmail.com>
2026-04-13 16:59:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 export const CONFIG = {
   BET_AMOUNT: 5,
+
+  // jumlah maksimal bet per cycle
   MAX_BETS: 3,
-  MAX_COINS: 10,
 
-  // ===== DIFF =====
-  MIN_DIFF_SHORT: 0.0025,  // 0.2% (CTM)
-  MIN_DIFF_LONG: 0.0025,   // 0.4% (Event)
+  // maksimal per coin (biar tidak spam BTC saja)
+  MAX_PER_COIN: 1,
 
-  // ===== TRIGGER =====
-  TRIGGER_SHORT: 20, // detik sebelum close
-  TRIGGER_LONG: 20,  // lebih aman untuk event
+  TRIGGER_SHORT: 30,
+  REFRESH_INTERVAL: 600000,
 
-  // ===== LIMIT =====
-  MAX_DELAY: 60 * 60, // 1 jam (short)
-
-  // ===== REFRESH =====
-  REFRESH_INTERVAL: 600000 // 10 menit
+  // diff per coin
+  DIFF: {
+    BTC: 0.002,
+    ETH: 0.0025,
+    SOL: 0.003,
+    BNB: 0.003,
+    CC: 0.004
+  }
 };
